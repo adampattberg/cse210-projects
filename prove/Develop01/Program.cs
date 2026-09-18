27f939fd52542ad21cd0c72b42f860dbf6f5ad8a
adampattberg <pat23012@byui.edu>
2026-09-17 19:13:20 -0500

    First C# Code

---
+++ prove/Develop01/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace game
+{
+    class Program
+    {
+        static void MAin(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is in C#.");
+        }
+    }
+    
+}
